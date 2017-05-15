42baef62ff81709e6f915bb241ce11d67a89600e
ASolidBPlus <onethousandandfourholds@gmail.com>
2017-05-16 00:10:12 +0930

    Create tourney.py
    main

---
+++ tourney.py
@@ -0,0 +1,26 @@
+import csv
+data = []
+with open ('fighters.csv', 'rb') as csvfile:
+        reader = csv.reader(csvfile, delimiter = ',')
+        for row in reader:
+                data.append([row[0], 0, [], []])
+
+with open ('fights.csv', 'rb') as csvfile:
+        i = 0
+        reader = csv.reader(csvfile, delimiter = ',')
+        for row in reader:
+                if (int(row[2]) == 0):
+                #First Fighter won
+                        data[int(row[0])][1] += 2 # Add 2 point per win
+                        data[int(row[0])][2].append(data[int(row[1])][0]) #Add loser to winners beaten list
+                        data[int(row[1])][3].append(data[int(row[0])][0]) #Add winner to losers lost to list
+                elif (int(row[2]) == 1):
+                        #Second Fighter won
+                        data[int(row[1])][1] += 2 # Add 2 point per win
+                        data[int(row[1])][2].append(data[int(row[0])][0]) #Add loser to winners beaten list
+                        data[int(row[0])][3].append(data[int(row[1])][0]) #Add winner to losers lost to list
+                else:
+                        #Error in data
+                        print "Data Error in row " + str(i)
+                i += 1
+print data[0]
