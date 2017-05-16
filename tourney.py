cf9abf563536f56e2bb6c4ee2d19131278f0ca0e
ASolidBPlus <onethousandandfourholds@gmail.com>
2017-05-16 04:58:03 +0200

    Updated sort function

--- tourney.py
+++ tourney.py
@@ -25,4 +25,5 @@ with open ('fights.csv', 'rb') as csvfile:
                         #Error in data
                         print "Data Error in row " + str(i)
                 i += 1
-print "Current standings:", sorted(data,key=itemgetter(1)) #Search by second value of 'data' list
+print "Current standings:", sorted(data,key=itemgetter(1), reverse=True) #Search by second value of 'data' list
+#test
